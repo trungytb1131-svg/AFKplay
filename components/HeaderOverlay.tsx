4510f0b63bee7c41aec685dc5bdddd48a608bfdf
afkplay <trungytb1131@gmail.com>
2026-05-18 11:29:06 +0700

    Mobile header: Logo 1x0.5 + Widget 1x0.5 stacked

--- components/HeaderOverlay.tsx
+++ components/HeaderOverlay.tsx
@@ -5,18 +5,39 @@ import PlayerHubWidget from "./PlayerHubWidget";
 
 export default function HeaderOverlay() {
   return (
-    <div className="fixed z-[200] top-[16px] left-[16px] right-[16px] pointer-events-none">
-      <div className="grid grid-cols-17 gap-[10px] w-full auto-rows-[calc((100vw-180px)/17)]">
-        {/* Logo (2x1) */}
-        <div className="col-span-2 row-span-1 pointer-events-auto">
-          <PortalLogo />
-        </div>
-
-        {/* PlayerHubWidget (2x2) dưới logo */}
-        <div className="col-span-2 row-span-2 col-start-1 row-start-2 pointer-events-auto">
-          <PlayerHubWidget />
+    <>
+      {/* ===== DESKTOP: 17 cột, Logo 2×1 + Widget 2×2 ===== */}
+      <div className="hidden lg:block fixed z-[200] top-[16px] left-[16px] right-[16px] pointer-events-none">
+        <div className="grid grid-cols-17 gap-[10px] w-full auto-rows-[calc((100vw-180px)/17)]">
+          <div className="col-span-2 row-span-1 pointer-events-auto">
+            <PortalLogo />
+          </div>
+          <div className="col-span-2 row-span-2 col-start-1 row-start-2 pointer-events-auto">
+            <PlayerHubWidget />
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* ===== MOBILE: Logo 1×0.5 + Widget 1×0.5 = combo 1×1 cố định ===== */}
+      <div
+        className="lg:hidden fixed z-[200] top-[10px] left-[10px] pointer-events-auto"
+        style={{ width: "calc((100vw - 40px) / 3)" }}
+      >
+        <div className="flex flex-col gap-[10px]">
+          <div
+            className="w-full rounded-[24px] overflow-hidden bg-white shadow-lg border border-white/50"
+            style={{ height: "calc((100vw - 40px) / 6)" }}
+          >
+            <PortalLogo />
+          </div>
+          <div
+            className="w-full rounded-[24px] overflow-hidden bg-white shadow-lg border border-white/50"
+            style={{ height: "calc((100vw - 40px) / 6)" }}
+          >
+            <PlayerHubWidget />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
