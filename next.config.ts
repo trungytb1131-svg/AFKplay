a8e254bed6e2689fe56900d75e0ffc5909507ced
afkplay <trungytb1131@gmail.com>
2026-05-19 12:51:52 +0700

    Remove self-hosted games, revert to DENY

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       {
         source: "/(.*)",
         headers: [
-          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           {
